e4a697bd2c4021e77cc3e6b1ec23d141e03c7473
m3trik@live.com <m3trik@live.com>
2026-04-20 22:42:05 -0400

    Update configs and prune old tests

--- test/temp_tests/old_tests/test_imports.py
+++
@@ -1,5 +0,0 @@
-from pythontk.test.test_module_resolver import validate_package, ModuleResolverValidator
-
-print("Imports successful")
-print(f"validate_package: {validate_package}")
-print(f"ModuleResolverValidator: {ModuleResolverValidator}")
